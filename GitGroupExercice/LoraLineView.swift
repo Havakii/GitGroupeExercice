eb96bc22c02f297fe043a6a6dea0e9debaa45d13
Lora-Line <loralinef@live.fr>
2025-10-21 15:03:34 +0200

    Create LoraLineView.swift

---
+++ GitGroupExercice/LoraLineView.swift
@@ -0,0 +1,20 @@
+//
+//  LoraLineView.swift
+//  GitGroupExercice
+//
+//  Created by apprenant75 on 21/10/2025.
+//
+
+import SwiftUI
+
+struct LoraLineView: View {
+    var body: some View {
+        Text("Lora-Line")
+            .font(Font.largeTitle.bold())
+            .foregroundStyle(Color.purple)
+    }
+}
+
+#Preview {
+    LoraLineView()
+}
