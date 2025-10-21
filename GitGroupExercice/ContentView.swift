6dfd3ae1e3dc9019c2be1705b77cae1003f08040
Havakii <havakii@hotmail.fr>
2025-10-21 14:37:56 +0200

    hava modif

--- GitGroupExercice/ContentView.swift
+++ GitGroupExercice/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, les Filles!")
+            Text("Hello, les Boss!")
         }
         .padding()
     }
