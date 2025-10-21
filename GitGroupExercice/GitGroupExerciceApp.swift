bbac09e6c58478f4450a28f3a020a3ad352f852d
Havakii <havakii@hotmail.fr>
2025-10-21 14:05:08 +0200

    first commit

---
+++ GitGroupExercice/GitGroupExerciceApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitGroupExerciceApp.swift
+//  GitGroupExercice
+//
+//  Created by Hava Bakrieva on 21/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct GitGroupExerciceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GitGroupExerciceTests/GitGroupExerciceTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitGroupExerciceTests.swift
+//  GitGroupExerciceTests
+//
+//  Created by Hava Bakrieva on 21/10/2025.
+//
+
+import Testing
+@testable import GitGroupExercice
+
+struct GitGroupExerciceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
