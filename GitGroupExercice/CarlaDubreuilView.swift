961f4dca42d1292f852d20e3bd29b7113082ec7b
Apprenant 83 <apprenant83@MacBook-Air-4.local>
2025-10-21 16:11:24 +0200

    merge Carla

---
+++ GitGroupExercice/CarlaDubreuilView.swift
@@ -0,0 +1,18 @@
+//
+//  CarlaDubreuilView.swift
+//  GitGroupExercice
+//
+//  Created by Apprenant 83 on 21/10/2025.
+//
+
+import SwiftUI
+
+struct CarlaDubreuilView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CarlaDubreuilView()
+}
