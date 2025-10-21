e3d118c483a8901c22b41b4a93172229f4fabc8e
Margot <toutousmargot@gmail.com>
2025-10-21 15:25:20 +0200

    Ajout page perso

---
+++ GitGroupExercice/MargotView.swift
@@ -0,0 +1,18 @@
+//
+//  MargotView.swift
+//  GitGroupExercice
+//
+//  Created by Apprenant 80 on 21/10/2025.
+//
+
+import SwiftUI
+
+struct MargotView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MargotView()
+}
